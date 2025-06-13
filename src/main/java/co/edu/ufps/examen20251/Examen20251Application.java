0633dc6c2354b8e4f43f0d329f6846a621b6ade3
julian <julianandresac@gmail.com>
2025-06-13 10:30:02 -0500

    primer commit

---
+++ src/main/java/co/edu/ufps/examen20251/Examen20251Application.java
@@ -0,0 +1,13 @@
+package co.edu.ufps.examen20251;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examen20251Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Examen20251Application.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/ufps/examen20251/Examen20251ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ufps.examen20251;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen20251ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
